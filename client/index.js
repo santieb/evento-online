ecbafa5642792b4e0d18aeb33b3610e8079410bb
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-01-25 21:43:26 -0300

    se logró

--- client/index.js
+++ client/index.js
@@ -1,13 +1,29 @@
 document.getElementById('send').addEventListener('click', () => {
   let data = {}
-  console.log("llego")
   data.name = document.getElementById('name').value;
-  data.username = document.getElementById('lastname').value;
+  data.lastname = document.getElementById('lastname').value;
   data.email = document.getElementById('email').value;
-
-  document.getElementById('lastname').value = ''
-  document.getElementById('name').value = ''
-  document.getElementById('email').value = ''
 
   newUser(data);
 })
+
+const newUser = async (dataInput) => {
+  let data = {
+      name: dataInput.name,
+      lastname: dataInput.lastname,
+      email: dataInput.email,
+  }
+
+  const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data),
+  }
+
+  await fetch(`http://localhost:5000/users/register`, requestOptions)
+      .then(res => res.json())
+      .then(res => console.log(res))
+      .catch((err) => console.log("err", err))
+}
