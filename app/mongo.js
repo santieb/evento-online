34fecb082bdad78d2b1bfb44e1fa608c195d2c19
Gustavo Sobol <gustavo.javier.sobol@gmail.com>
2022-01-25 23:00:53 -0300

    repair table mongo schema

--- app/mongo.js
+++ app/mongo.js
@@ -3,7 +3,6 @@ let mongoose = require("./config/conexion");
 const userSchema = require('./usuarios.model');
 
 const schema = new mongoose.Schema({ nombre: String, apellido: String, edad: Number });
-const Usuarios = mongoose.model("Usuarios", schema);
 
 const User = mongoose.model('users', userSchema);
 
