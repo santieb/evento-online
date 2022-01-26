51da7199cf69cdc1251abb3b02e86dcf723af7f7
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-01-25 22:33:40 -0300

    vamaa

--- client/index.js
+++ client/index.js
@@ -1,29 +1,26 @@
 document.getElementById('send').addEventListener('click', () => {
-  let data = {}
-  data.name = document.getElementById('name').value;
-  data.lastname = document.getElementById('lastname').value;
-  data.email = document.getElementById('email').value;
+  const data = {}
+  data.name = document.getElementById('name').value
+  data.lastname = document.getElementById('lastname').value
+  data.email = document.getElementById('email').value
 
-  newUser(data);
+  newUser(data)
 })
 
 const newUser = async (dataInput) => {
-  let data = {
-      name: dataInput.name,
-      lastname: dataInput.lastname,
-      email: dataInput.email,
-  }
+  const { name, lastname, email } = dataInput
+  const data = { name, lastname, email }
 
   const requestOptions = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data),
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
   }
 
-  await fetch(`http://localhost:5000/users/register`, requestOptions)
-      .then(res => res.json())
-      .then(res => console.log(res))
-      .catch((err) => console.log("err", err))
+  await fetch('http://localhost:5000/users/register', requestOptions)
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch((err) => console.log('err:', err))
 }
