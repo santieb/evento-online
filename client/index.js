3dab8607533ec83564bf383185e8df231770477b
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-01-25 21:22:48 -0300

    [add] advances

--- client/index.js
+++ client/index.js
@@ -1,13 +1,13 @@
 document.getElementById('send').addEventListener('click', () => {
-  const data = {}
-
-  data.name = document.getElementById('name').value
-  data.username = document.getElementById('lastname').value
-  data.email = document.getElementById('email').value
+  let data = {}
+  console.log("llego")
+  data.name = document.getElementById('name').value;
+  data.username = document.getElementById('lastname').value;
+  data.email = document.getElementById('email').value;
 
   document.getElementById('lastname').value = ''
   document.getElementById('name').value = ''
   document.getElementById('email').value = ''
 
-  newUser(data)
+  newUser(data);
 })
--- client/script.js
+++ client/script.js
@@ -1,27 +1,24 @@
-const msj = document.getElementById('msj')
-const app_port = 4000
-
 const newUser = async (dataInput) => {
-  let resp
-  const myHeaders = new Headers()
-  myHeaders.append('Content-Type', 'application/json')
-
-  const data = {
-    username: dataInput.name,
-    name: dataInput.lastname,
-    email: dataInput.email
+  let resp;
+  let myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  console.log(dataInput);
+  let data = {
+      username: dataInput.name,
+      name: dataInput.lastname,
+      email: dataInput.email,
   }
 
   const requestOptions = {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: myHeaders,
-    redirect: 'follow'
-  }
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: myHeaders,
+      redirect: 'follow'
+  };
 
-  await fetch(`http://localhost:${app_port}/users`, requestOptions)
-    .then(response => response.text())
-    .then(result => resp = result)
-    .catch(error => console.log('error', error))
-  return resp
+  await fetch(`http://localhost:5000/users/register`, requestOptions)
+      .then(response => response.text())
+      .then(result => resp = result)
+      .catch(error => console.log('error', error));
+  return resp;
 }
